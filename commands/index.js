efc28ad4130ee29ef5d9949c05702744ff20130f
Luckz21 <wender2130@gmail.com>
2026-04-13 18:29:03 -0400

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,4 @@
 // commands/index.js
-// Este archivo re-exporta todos los comandos y utilidades públicas
-
-// Importar todos los módulos de comandos
 const verification = require('./verification');
 const profile = require('./profile');
 const robloxCommands = require('./robloxCommands');
@@ -16,48 +13,20 @@ const owner = require('./owner');
 const help = require('./help');
 const monitor = require('./monitor');
 
-// Re-exportar todo
 module.exports = {
-  // Verificación
   ...verification,
-
-  // Perfil
   ...profile,
-
-  // Roblox y catálogo
   ...robloxCommands,
-
-  // Premium
   ...premium,
-
-  // Economía
   ...economy,
-
-  // Trivia
   ...triviaCommands,
-
-  // Social
   ...social,
-
-  // Moderación
   ...moderation,
-
-  // Alertas y privacidad
   ...alerts,
-
-  // Admin
   ...admin,
-
-  // Owner
   ...owner,
-
-  // Ayuda
   ...help,
-
-  // Monitor y eventos
   ...monitor,
-
-  // Cooldowns (desde security)
   cooldowns: require('../security').cooldowns,
   CooldownManager: require('../security').CooldownManager
 };
